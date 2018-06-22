420a63ef6e193aa648ee61f3273296016ae139c1
eleanormarshall <eleanor.rae.marshall@gmail.com>
2018-06-22 11:28:59 -0500

    added comments to cleanpaths.js file

--- Client-Side/cleanpaths.js
+++ Client-Side/cleanpaths.js
@@ -2,6 +2,7 @@
 
 //Universal functions & Variables used for all three paths
 
+//getPaths makes the GET request for the paths
 function getPaths(url, callback){ 
 	var client = new XMLHttpRequest(); 
 	client.open('GET', url); 
@@ -11,10 +12,12 @@ function getPaths(url, callback){
 	client.send(); 
 	} 
 
+//defines a new variable to communicate that the path data will be in the specific IGC formatting 
 var igcFormat = new ol.format.IGC(); 
 
-//WB9SKY Layer 
+//WB9SKY Layer - all three layers are the same code. WB9SKY represents the potential APRS callsign.  
 
+//defines the style for the WB9SKY layer
 var WB9SKYstyle = new ol.style.Style({ 
 		stroke: new ol.style.Stroke({ 
 			color: 'rgba(255, 17, 97, 0.7)', 
@@ -22,11 +25,13 @@ var WB9SKYstyle = new ol.style.Style({
 			})
 		}); 
 
-
+//creates a vector source for the WB9SKY layer
 var WB9SKYvectorSource = new ol.source.Vector(); 
 
+//defines the URL to call for the WB9SKY layer 
 var WB9SKYurl = ['http://192.168.1.1/cgi-bin/TrackerV1Codes/paths.py?callsign=WB9SKY-11']; 
 
+//adds the path and features to the WB9SKYlayer 
 var WB9SKYfetchdata = function(){ 
 	getPaths(WB9SKYurl, function(data){ 
 		var WB9SKYfeatures = igcFormat.readFeatures(data, {featureProjection: 'EPSG:3857'}); 
@@ -37,17 +42,18 @@ var WB9SKYfetchdata = function(){
 
 	updateTimer = setTimeout(function(){  
 		WB9SKYfetchdata(); 
-		}, 1000)}; 
-
+		}, 1000)};
+ 
+//runs the WB9SKYfetchdata function  
 WB9SKYfetchdata(); 
 
-
+//adds the vector source and style to the WB9SKY layer 
 var WB9SKY = new ol.layer.Vector({ 
 		source: WB9SKYvectorSource, 
 		style: WB9SKYstyle
 		}); 
 
-//KC9LHW Layer
+//KC9LHW Layer - refer to the comments in the WB9SKY layer for information about the KC9LHW & KC9LIG layers 
 
 var KC9LHWstyle = new ol.style.Style({ 
 		stroke: new ol.style.Stroke({ 
@@ -116,3 +122,4 @@ var KC9LIG = new ol.layer.Vector({
 		}); 
 
 			
+
